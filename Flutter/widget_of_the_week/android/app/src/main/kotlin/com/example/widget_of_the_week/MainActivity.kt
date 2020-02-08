6c655661d8153b80bfcb89b0cbcbfcb1ca175de2
Luke Chi <luke.chi@gmail.com>
2020-02-08 16:46:00 +0800

    add

---
+++ Flutter/widget_of_the_week/android/app/src/main/kotlin/com/example/widget_of_the_week/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.widget_of_the_week
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
