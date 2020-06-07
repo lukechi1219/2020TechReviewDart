5ea9667d2ad88ae625a0c28816ddae757de8a5b0
Luke Chi <luke.chi@gmail.com>
2020-06-08 06:47:52 +0800

    add

---
+++ Flutter/clean_architecture_tdd_final/lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -0,0 +1,16 @@
+import 'package:clean_architecture_tdd_course/core/error/failure.dart';
+import 'package:clean_architecture_tdd_course/core/usecases/usecase.dart';
+import 'package:clean_architecture_tdd_course/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:clean_architecture_tdd_course/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class GetRandomNumberTrivia extends UseCase<NumberTrivia, NoParams> {
+  final NumberTriviaRepository repository;
+
+  GetRandomNumberTrivia(this.repository);
+
+  @override
+  Future<Either<Failure, NumberTrivia>> call(NoParams params) async {
+    return await repository.getRandomNumberTrivia();
+  }
+}
