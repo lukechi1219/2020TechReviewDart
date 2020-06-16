00815a30a6b3c2a0401650bdfa8822a556476d87
Luke Chi <luke.chi@gmail.com>
2020-06-17 00:51:52 +0800

    add

--- Flutter/clean_architecture_tdd_final/lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
+++ Flutter/clean_architecture_tdd_final/lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 import 'package:clean_architecture_tdd_course/core/error/exceptions.dart';
 import 'package:clean_architecture_tdd_course/features/number_trivia/data/models/number_trivia_model.dart';
-import 'package:clean_architecture_tdd_course/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:meta/meta.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'number_trivia_remote_data_source.dart';
 
 abstract class NumberTriviaLocalDataSource {
   Future<NumberTriviaModel> getLastNumberTrivia();
@@ -33,5 +36,35 @@ class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
       CACHED_NUMBER_TRIVIA,
       json.encode(triviaToCache.toJson()),
     );
+  }
+}
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
+  final http.Client client;
+
+  NumberTriviaRemoteDataSourceImpl({@required this.client});
+
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) async {
+    final response = await client.get(
+      'http://numbersapi.com/$number',
+      headers: {'Content-Type': 'application/json'},
+    );
+    if (response.statusCode == 200) {
+      return NumberTriviaModel.fromJson(json.decode(response.body));
+    }
+    throw ServerException();
+  }
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() async {
+    final response = await client.get(
+      'http://numbersapi.com/random',
+      headers: {'Content-Type': 'application/json'},
+    );
+    if (response.statusCode == 200) {
+      return NumberTriviaModel.fromJson(json.decode(response.body));
+    }
+    throw ServerException();
   }
 }
