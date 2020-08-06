f92be769b540c2c7d10ae92f5cadecda975f7d54
Luke Chi <luke.chi@gmail.com>
2020-08-07 01:10:00 +0800

    Update main.dart

--- Flutter/clean_architecture_tdd_final/lib/main.dart
+++ Flutter/clean_architecture_tdd_final/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'injection_container.dart' as di;
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await di.init();
   runApp(MyApp());
 }
