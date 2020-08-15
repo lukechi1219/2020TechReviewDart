32297be1614185f3485fc489c4a6c14dea4d6475
Luke Chi <luke.chi@gmail.com>
2020-08-15 17:56:51 +0800

    Update trivia_display.dart

--- Flutter/clean_architecture_tdd_final/lib/features/number_trivia/presentation/widgets/trivia_display.dart
+++ Flutter/clean_architecture_tdd_final/lib/features/number_trivia/presentation/widgets/trivia_display.dart
@@ -1,9 +1,38 @@
 import 'package:clean_architecture_tdd_final/features/number_trivia/domain/entities/number_trivia.dart';
-import 'package:clean_architecture_tdd_final/features/number_trivia/presentation/bloc/bloc.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
-BlocProvider<NumberTriviaBloc> TriviaDisplay(
-    {@required NumberTrivia numberTrivia}) {
-  return null;
+class TriviaDisplay extends StatelessWidget {
+  final NumberTrivia numberTrivia;
+
+  const TriviaDisplay({
+    Key key,
+    @required this.numberTrivia,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 3,
+      child: Column(
+        children: <Widget>[
+          Text(
+            numberTrivia.number.toString(),
+            style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+          ),
+          Expanded(
+            child: Center(
+              child: SingleChildScrollView(
+                child: Text(
+                  numberTrivia.text,
+                  style: TextStyle(fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
