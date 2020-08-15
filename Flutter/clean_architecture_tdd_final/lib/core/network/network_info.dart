3963f4a34749cc49a9f3ccecc48ac5b08c297773
Luke Chi <luke.chi@gmail.com>
2020-08-15 17:45:12 +0800

    Update network_info.dart

--- Flutter/clean_architecture_tdd_final/lib/core/network/network_info.dart
+++ Flutter/clean_architecture_tdd_final/lib/core/network/network_info.dart
@@ -1,4 +1,5 @@
 import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:flutter/foundation.dart';
 
 abstract class NetworkInfo {
   Future<bool> get isConnected;
@@ -10,5 +11,6 @@ class NetworkInfoImpl implements NetworkInfo {
   NetworkInfoImpl(this.connectionChecker);
 
   @override
-  Future<bool> get isConnected => connectionChecker.hasConnection;
+  Future<bool> get isConnected =>
+      kIsWeb ? true : connectionChecker.hasConnection;
 }
